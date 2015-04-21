852bbb85c2c2a1aeaf8ec91646e9a73bb067ede3
andremn <andremn@ciandt.com>
2015-04-21 20:54:58 -0300

    Criação do RootController

---
+++ SPS.Web/Controllers/RootController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SPS.Web.Controllers
+{
+    public class RootController : Controller
+    {
+        // GET: Root
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ SPS.Web/Models/RootUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SPS.Web.Models
+{
+    public class RootUserViewModel
+    {
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
