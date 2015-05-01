ce2d101108a61a3b3346302cea9ed6bc26272673
andremn <andremn@ciandt.com>
2015-05-01 18:55:48 -0300

    Adicionado ParkingManagement view

--- SPS.Web/Controllers/RootController.cs
+++ SPS.Web/Controllers/RootController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SPS.Repository;
+using SPS.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,9 +16,27 @@ namespace SPS.Web.Controllers
             return View();
         }
 
-        public ActionResult Login()
+        //
+        // POST: /Root/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(RootUserViewModel rootUser)
         {
-            return RedirectToAction("Index", "Home");
+            var user = SPSContext.Instance.Root.Find(rootUser.Token);
+
+            if (user == null)
+            {
+                return View();
+            }
+
+            HttpContext.GetOwinContext().Authentication.SignOut();
+            return RedirectToAction("ParkingManagement", "Root");
+        }
+
+        public ActionResult ParkingManagement()
+        {
+            return View();
         }
     }
 }
