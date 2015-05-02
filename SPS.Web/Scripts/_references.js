3ff1694d53e2e038f024e474661053833fd938da
casarini12 <hugoc@ciandt.com>
2015-05-02 09:51:45 -0300

    Correções na Arquitetura

--- SPS.Web/Scripts/_references.js
+++ SPS.Web/Scripts/_references.js
@@ -1,9 +1,9 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="jquery-1.10.2.js" />
+/// <reference path="jquery.mask.min.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="npm.js" />
 /// <reference path="register.js" />
-/// <reference path="jquery.mask.min.js" />
 
 
 function corrigirFooter(elemento, tamanhoMargem) {
