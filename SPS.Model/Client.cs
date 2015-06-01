7cfb7e384e35b8b80480b22d3c0e13cc61775372
casarini12 <hugoc@ciandt.com>
2015-05-31 23:05:11 -0300

    acertos gerais de bugs e correções

---
+++ SPS.Model/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace SPS.Model
+{
+    public class Client : User
+    {
+        public virtual IList<Tag> Tags { get; set; }
+
+        public virtual IList<UsageRecord> Records { get; set; }
+
+        public virtual IList<Parking> Parkings { get; set; }
+    }
+}
---
+++ SPS.Web/Scripts/requestNewTag.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("#requestTagLink").on('click', function () {
+        $('#confirmRequestModal').modal('show');
+    });
+
+    $('#confirmRequestBtn').click(function () {
+        $.post("/Collaborator/RequestNewTag").done(function () {
+            alert("Ok");
+        });
+    });
+});
