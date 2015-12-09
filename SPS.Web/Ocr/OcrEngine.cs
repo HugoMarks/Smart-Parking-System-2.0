b78d7db3196edefcf41283e5a1d85414aeac850e
André Nicoletti <andre_nls@live.com>
2015-12-08 23:08:24 -0200

    OcrEngine.

---
+++ SPS.Web/Ocr/OcrEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace SPS.Web.Ocr
+{
+    public class OcrEngine
+    {
+        public OcrEngine()
+        {
+
+        }
+
+        public string GetText(Bitmap image)
+        {
+            var pixImage = PixConverter.ToPix(image);
+            var tesseract = new Tesseract();
+
+            tesseract.
+        }
+    }
+}
